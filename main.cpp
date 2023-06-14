44fe060d5c5757785dc7164975efa435447c901b
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2023-06-14 22:44:47 +0200

    Implemented timer feature, fixed SOL arranging

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <iomanip>
 #include "SkipList.h"
+#include "Timer.h"
 
 
 int main() {
+    Timer t;
     SkipList<int> skipList(10);
 
     // Insert elements into the skip list
@@ -15,12 +18,15 @@ int main() {
 
     // Search for elements in the skip list
     int value = 4;
+    t.start();
     bool found = skipList.search(value);
+    t.stop();
     if (found) {
         std::cout << "Element " << value << " jest w skip liscie." << std::endl;
     } else {
         std::cout << "Element " << value << " nie znaleziony." << std::endl;
     }
+    std::cout << "trwalo " << std::fixed << std::setprecision(10) << t.elapsedMilliseconds() << " ms";
 
     return 0;
 }
